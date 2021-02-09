3be0440644861197252ba1189c8cee2ea0618599
jhon <jhont285@gmail.com>
2021-02-08 20:31:22 -0500

    improving log and added xaws

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -5,11 +5,16 @@ import todosLogic from "../../businessLogic/todosLogic";
 import {StatusCodes} from "http-status-codes";
 import * as middy from "middy";
 import {cors} from "middy/middlewares";
+import {getLogger} from "../../utils/logger";
+
+const log = getLogger();
 
 const main: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const userId = event.requestContext.authorizer.principalId as string;
 
+  log.info("getTodos: " + JSON.stringify({userId}, null, 4))
   const items = await todosLogic.getAll(userId);
+  log.info(`getTodos: Successful operation ${JSON.stringify({items}, null, 4)}`);
   return {
     statusCode: StatusCodes.OK,
     body: JSON.stringify({items}),
